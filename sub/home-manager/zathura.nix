746b59433d3a1ef3d4a772589c9a30d280e0454d
tautastic <tautastic@proton.me>
2026-08-04 13:02:56 +0200

    Fix Zathura keybinds

--- sub/home-manager/zathura.nix
+++ sub/home-manager/zathura.nix
@@ -1,3 +1,10 @@
+{ lib, ... }:
+
+let
+  allModes = [ "" "index" "fullscreen" "presentation" ];
+  mapForModes   = key: action: modes: lib.listToAttrs (map (mode: { name = if mode == "default" || mode == "" then key else "[${mode}] ${key}"; value = action; }) modes);
+  unmapForModes = key: modes: lib.concatStringsSep "\n" (map (mode: "unmap ${if mode == "default" || mode == "" then key else "[${mode}] ${key}"}") modes);
+in
 {
   programs.zathura = {
     enable = true;
@@ -7,9 +14,16 @@
       guioptions = "s";
     };
 
-    mappings = {
-      "<C-f>" = "fullscreen";
-      "<C-p>" = "presentation";
-    };
+    mappings =
+      mapForModes "f" "follow" allModes
+      // mapForModes "F" "display_link" allModes
+      // mapForModes "<C-f>" "toggle_fullscreen" [ "" "index" "fullscreen" ]
+      // mapForModes "<C-p>" "toggle_presentation" [ "" "index" "presentation" ];
+
+    extraConfig = ''
+      ${unmapForModes "F5" allModes}
+      ${unmapForModes "F11" allModes}
+      set selection-clipboard clipboard
+    '';
   };
 }
